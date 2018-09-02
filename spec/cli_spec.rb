ac5875ab5f76b3947c7d415eec85f3fe975ea313
Manabu Niseki <manabu.niseki@gmail.com>
2018-09-03 07:33:22 +0900

    feat: add --user_agent feature

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -1,46 +1,54 @@
 # frozen_string_literal: true
 
 require "json"
+
 vcr_options = { cassette_name: "cli", record: :new_episodes }
 RSpec.describe WhatWeb::CLI, vcr: vcr_options do
+  subject { WhatWeb::CLI }
   context "without the aggressive mode" do
     describe "#execute_plugins" do
-      subject { WhatWeb::CLI.new }
       it "should return a Hash" do
-        results = subject.execute_plugins "https://github.com"
+        results = subject.new.execute_plugins "https://www.webscantest.com"
         expect(results).to be_a(Hash)
       end
     end
     describe "#scan" do
-      subject { WhatWeb::CLI }
       it "should output a JSON" do
-        output = capture(:stdout) { subject.start %w(scan https://github.com) }
+        output = capture(:stdout) { subject.start %w(scan https://www.webscantest.com) }
         json = JSON.parse(output)
         expect(json).to be_a(Hash)
-        expect(json.dig("Title").first.dig("string")).to eq("The world’s leading software development platform · GitHub")
+        expect(json.dig("Title").first.dig("string")).to eq("Test Site")
       end
     end
   end
   context "with the aggressive mode" do
     describe "#execute_plugins" do
-      subject { WhatWeb::CLI.new }
       it "should return a Hash" do
-        results = subject.execute_plugins "https://github.com"
+        results = subject.new.execute_plugins("https://www.webscantest.com", is_aggressive: true)
         expect(results).to be_a(Hash)
       end
     end
     describe "#scan" do
-      subject { WhatWeb::CLI }
       it "should output a JSON" do
-        output = capture(:stdout) { subject.start %w(scan https://github.com) }
+        output = capture(:stdout) { subject.start %w(scan https://www.webscantest.com --aggressive) }
         json = JSON.parse(output)
         expect(json).to be_a(Hash)
       end
     end
   end
 
-  describe "list_plugins" do
-    subject { WhatWeb::CLI }
+  context "with a custom user_agent" do
+    describe "#scan" do
+      it "should not raise any error" do
+        ## I don't know how to test HTTP request header value...
+        expect {
+          capture(:stdout) { subject.start %w(scan https://www.webscantest.com --user_agent=hoge) }
+        }.not_to raise_error
+      end
+    end
+  end
+
+  describe "#list_plugins" do
     it "should output an Array " do
       output = capture(:stdout) { subject.start %w(list_plugins) }
       json = JSON.parse(output)
